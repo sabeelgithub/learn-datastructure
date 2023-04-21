77093722342451d18a31689b4d7fd2b9f3610291
mohammedsabeel <mohammedsabeeltc786@gmail.com>
2023-04-21 20:13:27 +1300

    working on heap

--- DS-3.py
+++ DS-3.py
@@ -1,3 +1,4 @@
+################# BST ##############################
 class BST:
     def __init__(self,key):
         self.key = key
@@ -67,64 +68,147 @@ class BST:
         print(self.key,end='--->')
 
     # delete node in BST
-    def delete(self,data):
+    def delete(self,data,curr):
         if self.key is None:
             print('BST is empty')
             return
         if data < self.key:
             if self.lchild:
-                self.lchild = self.lchild.delete(data)
+                self.lchild = self.lchild.delete(data,curr)
             else:
                 print(f'Given node {data} not found in BST')
 
         elif data > self.key:
             if self.rchild:
-                self.rchild = self.rchild.delete(data)
+                self.rchild = self.rchild.delete(data,curr)
             else:
                 print(f'Given node {data} not found in BST')
         else:
             if self.lchild is None:
                 temp = self.rchild
+                if data == curr:
+                    self.key = temp.key
+                    self.lchild = temp.lchild
+                    self.rchild = temp.rchild
+                    temp = None
+                    return
                 self = None
                 return temp
             if self.rchild is None:
                 temp = self.lchild
+                if data == curr:
+                    self.key = temp.key
+                    self.lchild = temp.lchild
+                    self.rchild = temp.rchild
+                    temp = None
+                    return
                 self = None
                 return temp
             node = self.rchild
             while node.lchild:
                 node = self.lchild
             self.key = node.key
-            self.rchild = self.rchild.delete(node.key)
+            self.rchild = self.rchild.delete(node.key,curr)
         return self
+    
+    # to find minimum node in the given BST
+    def min_node(self):
+        current = self
+        while current.lchild:
+            current = current.lchild
+        print('node with smallest key is:',current.key)
+
+    # to find maximum node in the given BST
+    def max_node(self):
+        current = self
+        while current.rchild:
+            current = current.rchild
+        print('node with largest key is:',current.key)
 
 
 
     
-               
+def count(node):
+    if node is None:
+        return 0
+    return 1+count(node.lchild)+count(node.rchild)               
 
 
-root = BST(None)
-print(root.key)
-list1 = [10,5,15,3,20]
+root = BST(10)
+list1 = [1,2,100]
 for i in list1:
     root.insert(i)
-
 print(root.key)
-print(root.lchild)
-print(root.rchild)
-root.search(10)
+print(count(root))
+if count(root)>1:
+    root.delete(10,root.key)
+else:
+    print('cant perform deletion operation')
 
-print('pre-order traversal')
+print('pre-order')
 root.preOrder()
 print()
-print('in-order traversal')
-root.inOrder()
+root.min_node()
 print()
-print('post-order traversal')
-root.postOrder()
-print()
-print('delete')
-root.delete(10)
-root.preOrder()
+root.max_node()
 
+
+# print(root.key)
+# print(root.lchild)
+# print(root.rchild)
+# root.search(10)
+
+# print('pre-order traversal')
+# root.preOrder()
+# print()
+# print('in-order traversal')
+# root.inOrder()
+# print()
+# print('post-order traversal')
+# root.postOrder()
+# print()
+# print('delete')
+# root.delete(10)
+# root.preOrder()
+
+#################### END BST ##########################
+
+
+########################### HEAP ##########################
+
+# ----------- BINARY HEAP ------------------#
+
+# implementation using heapq module
+
+import heapq
+heap = []
+heapq.heappush(heap,10)
+print(heap)
+heapq.heappush(heap,1)
+print(heap)
+heapq.heappush(heap,5)
+print(heap)
+heapq.heappop(heap)
+print(heap)
+heapq.heappop(heap)
+print(heap)
+heapq.heappop(heap)
+print(heap)
+list1 = [1,3,5,2,4,6]
+heapq.heapify(list1)
+print(list1)
+print(heapq.heappushpop(list1,89))
+print(list1)
+print(heapq.heapreplace(list1,100))
+print(list1)
+
+print(heapq.nsmallest(2,list1))
+print(heapq.nlargest(3,list1))
+
+list = [(1,'ashu'),(2,'ann'),(3,'konn')]
+heapq.heapify(list)
+print(list)
+for i in range(len(list)):
+    print(heapq.heappop(list))
+
+########################### END HEAP ############################
