112160febde6a82d57b38b44e7ab29ef4e71a5c4
mohammedsabeel <mohammedsabeeltc786@gmail.com>
2023-07-19 07:07:23 +1300

    DS-2 completed

--- DS-2/Sorting/insertion_sort.py
+++ DS-2/Sorting/insertion_sort.py
@@ -4,9 +4,9 @@ print(array,'orginal array')
 def insertionSort(arr):
     for i in range(len(arr)-1):
         key = i
-        while i>0 and arr[key-1]>arr[key]:
+        while key>0 and arr[key-1]>arr[key]:
             arr[key-1],arr[key]=arr[key],arr[key-1]
-            i -= 1
+            key -= 1
     return arr
 insertion_sorted = insertionSort(array)
 print(insertion_sorted,'insertion sorted')
